eb6290635b9c604cabe1282d0e6cb8ee91aede9f
arturo <arturo@ArturoMSI>
2018-03-01 10:32:59 +0100

    Añadido Javadoc

--- src/reloj/Alarma.java
+++ src/reloj/Alarma.java
@@ -3,6 +3,7 @@ package reloj;
 import javax.swing.JOptionPane;
 
 /**
+ * La clase alarma permite crear una alarma para que suene el reloj
  *
  * @author Arturo
  */
@@ -11,16 +12,30 @@ public class Alarma{
     private int horaA, minutoA;
     private boolean estado=false;
 
+    /**
+     * Constructor de la clase Alarma, inicia la alarma a una hora por defecto
+     */
     public Alarma(){
         horaA=12;
         minutoA=00;
     }
 
+    /**
+     * Metodo de acceso que permite el estado de la alarma
+     *
+     * @return un valor boolean si la alarma está activa o no
+     */
     public boolean isEstado(){
         return estado;
     }
-    
 
+    /**
+     * Metodo que cambia el estado de la alarma, si estaba activo lo desactiva y
+     * si estaba desactivado la activa
+     *
+     * Tambien muestra un mensaje por pantalla para conocer el estado del cambio
+     * (este mensaje es temporal)
+     */
     public void activarDesactivarAlarma(){
         if(estado==false){
             estado=true;
@@ -31,6 +46,10 @@ public class Alarma{
         }
     }
 
+    /**
+     * Este metodo permite cambiar la alarma mediante una selecion. El valor de
+     * la alarma se incrementa de uno en uno
+     */
     public void cambiarHoraAlarma(){
         int selec;
         do{
@@ -38,7 +57,8 @@ public class Alarma{
                     +"1-- Sumar horas\n"
                     +"2-- Sumar minutos\n"
                     +"0-- Salir"
-                    +"\n\n"+horaA+":"+minutoA));
+                    +"\n\n"
+                    +horaA+":"+minutoA));//Muestra el valor actual de la alarma
 
             switch(selec){
                 case 1:
@@ -46,14 +66,14 @@ public class Alarma{
                         horaA++;
                     }else{
                         horaA=0;
-                    }
+                    }//evita que la hora se pase de 23 y devuelve el valor a 0
                     break;
                 case 2:
                     if(minutoA<59){
                         minutoA++;
                     }else{
                         minutoA=0;
-                    }
+                    }//evita que los minutos superen 59 y devuelve su valor a 0
                     break;
             }
         }while(selec!=0);
