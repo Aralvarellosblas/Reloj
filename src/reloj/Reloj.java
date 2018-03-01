01651fcdcbd96af42b8f4c08a89c42957ebbbb8c
arturo <arturo@ArturoMSI>
2018-03-01 10:23:36 +0100

    Añadido Javadoc

--- src/reloj/Reloj.java
+++ src/reloj/Reloj.java
@@ -3,12 +3,15 @@ package reloj;
 import javax.swing.JOptionPane;
 
 /**
+ * Clase principar del programa
  *
  * @author Arturo
  */
 public class Reloj{
 
     /**
+     * Metodo Main contiene el switch para la selecion de los metodos
+     *
      * @param args the command line arguments
      */
     public static void main(String[] args){
@@ -16,16 +19,16 @@ public class Reloj{
         int s=Integer.parseInt(JOptionPane.showInputDialog("1-- Estado de alarma"
                 +"\n2 Reloj"));
 //        do{
-            switch(s){
-                case 1:
-                    al.activarDesactivarAlarma();
-                    break;
-                case 2:
-                    Hora.tiempo();
-                    break;
-                default:
-                    System.out.println("Seleccion Incorrecta");
-            }
+        switch(s){
+            case 1:
+                al.activarDesactivarAlarma();
+                break;
+            case 2:
+                Hora.tiempo();
+                break;
+            default:
+                System.out.println("Seleccion Incorrecta");
+        }
 //        }while(s!=0);
     }
 
