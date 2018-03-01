b181e4233386700f846c429ac8d507102f47aa23
arturo <arturo@ArturoMSI>
2018-03-01 10:19:30 +0100

    Añadido Javadoc

--- src/reloj/Display.java
+++ src/reloj/Display.java
@@ -1,6 +1,5 @@
 package reloj;
 
-import java.util.Date;
 import javax.swing.JOptionPane;
 
 /**
@@ -9,16 +8,34 @@ import javax.swing.JOptionPane;
  */
 public class Display{
 
+    /**
+     * Metodo que muestra la hora actual del Reloj
+     *
+     * @param h hora
+     * @param m minutos
+     * @param s segundos
+     * @param alarma valor que permite saber si la alarma está activada o no
+     */
     public static void mostrarHora(int h, int m, int s, boolean alarma){
         if(alarma==true){
             System.out.println(h+":"+m+":"+s+" Alarm");
-        }else System.out.println(h+":"+m+":"+s);
+        }else{
+            System.out.println(h+":"+m+":"+s);
+        }
     }
-
+    /**
+     * Metodo que permite mostrar mensajes de información
+     * @param m contenido del mensaje
+     */
     public static void mostrarMensaje(String m){
         JOptionPane.showMessageDialog(null, m);
     }
-
+    /**
+     * Metodo que permite que se muestre la hora a la que está puesta la alarma
+     * 
+     * @param ah hora de la alarma
+     * @param am minuto de la alarma
+     */
     public static void mostrarAlarma(int ah, int am){
         System.out.println(ah+":"+am);
     }
