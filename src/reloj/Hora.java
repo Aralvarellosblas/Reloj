58f9d3f48ae3cb585ec7856230134a1db8558fa0
arturo <arturo@ArturoMSI>
2018-03-15 10:08:04 +0100

    Clase Hora terminada

--- src/reloj/Hora.java
+++ src/reloj/Hora.java
@@ -1,6 +1,8 @@
 package reloj;
 
 import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -11,27 +13,33 @@ import java.util.logging.Logger;
  */
 public class Hora{
 
-    private static int h, m, s;
+    protected static int h, m, s;
     private static Date d;
+    private static Alarma al;
+    private static Timer t;
 
     /**
+     * 
      * Este metodo obtiene la fecha del sistema y muestra la hora minuto y
      * segundo para formar el reloj
+     * 
      */
-    public static void tiempo(){
-        Alarma al=new Alarma();
+    public void tiempo(){
+        t=new Timer();
+        al=new Alarma();
         do{
             try{
                 d=new Date();
                 h=d.getHours();
                 m=d.getMinutes();
                 s=d.getSeconds();
-                Display.mostrarHora(h, m, s, al.isEstado());
+                Display.mostrarHora(h, m, s);
+                t.schedule(new Alarma(), 0, 1000);
                 Thread.sleep(1000);
             }catch(InterruptedException ex){
                 Logger.getLogger(Hora.class.getName()).log(Level.SEVERE, null, ex);
             }
         }while(1==1);
-
     }
+
 }
