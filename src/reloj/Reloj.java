fdb44d3d8c806b708433aabbd2cecc8e31806e69
arturo <arturo@ArturoMSI>
2018-03-15 10:08:17 +0100

    Clase Main terminada

--- src/reloj/Reloj.java
+++ src/reloj/Reloj.java
@@ -10,26 +10,33 @@ import javax.swing.JOptionPane;
 public class Reloj{
 
     /**
+     * 
      * Metodo Main contiene el switch para la selecion de los metodos
      *
      * @param args the command line arguments
      */
     public static void main(String[] args){
         Alarma al=new Alarma();
-        int s=Integer.parseInt(JOptionPane.showInputDialog("1-- Estado de alarma"
-                +"\n2 Reloj"));
-//        do{
-        switch(s){
-            case 1:
-                al.activarDesactivarAlarma();
-                break;
-            case 2:
-                Hora.tiempo();
-                break;
-            default:
-                System.out.println("Seleccion Incorrecta");
-        }
-//        }while(s!=0);
+        Hora ho=new Hora();
+        int s;
+        do{
+            s=Integer.parseInt(JOptionPane.showInputDialog("1-- Estado Alarma"
+                    +"\n2-- Reloj"
+                    +"\n3-- Poner hora alarma"));
+            switch(s){
+                case 1:
+                    al.activarDesactivarAlarma();
+                    break;
+                case 2:
+                    ho.tiempo();
+                    break;
+                case 3:
+                    Alarma.cambiarHoraAlarma();
+                    break;
+                default:
+                    System.out.println("Seleccion Incorrecta");
+            }
+        }while(true);
     }
 
 }
